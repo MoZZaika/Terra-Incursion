317207c41db39c42365f37add5ca45e52f66e48b
Dr.Marx <AlexsSyr@mail.ru>
2023-01-30 14:31:30 +0300

    Init

---
+++ TerraIncursion/TerraIncursionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TerraIncursionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATerraIncursionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATerraIncursionGameMode();
+};
+
+
+
